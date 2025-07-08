a63c7023dbe56c36f68f1814bc3443722bad9989
Yash Yadav <yyash9974@gmail.com>
2025-07-08 13:06:26 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-jhcvvj
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 190)
+
+
+def speak(command):
+    engine.say(command)
+    engine.runAndWait()
+
+speak("How are you?")
